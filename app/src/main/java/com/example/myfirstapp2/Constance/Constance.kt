7e2c8f7b54249aea633d0b3127ada8ab32d46784
Alex Pankov <boxpen7@gmail.com>
2023-08-05 17:38:48 +0200

    Changed user name

--- app/src/main/java/com/example/myfirstapp2/Constance/Constance.kt
+++ app/src/main/java/com/example/myfirstapp2/Constance/Constance.kt
@@ -1,13 +1,13 @@
 package com.example.myfirstapp2.Constance
 
 object Constance {
-    const val DIRECTOR_SUELDO = 100000  // Egor
-    const val INGENER_SUELDO = 10000    // Andrey
-    const val DVORNIK_SUELDO = 1000     // Sergey
+    const val DIRECTOR_SUELDO = "100.000 EUR"   // Aino
+    const val INGENER_SUELDO = "10.000 Tenge"   // Andrey
+    const val DVORNIK_SUELDO = 10.000           // Fernando
 
-    const val DIRECTOR = "Egor"
+    const val DIRECTOR = "Aino"
     const val INGENER = "Andrey"
-    const val DVORNIK = "Sergey"
+    const val DVORNIK = "Fernando"
 
     const val DIRECTOR_PASSWORD = "4565"
     const val INGENER_PASSWORD = "1232"
